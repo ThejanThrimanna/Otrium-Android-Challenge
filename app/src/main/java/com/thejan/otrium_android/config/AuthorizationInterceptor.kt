96195954ef1d5bea75abb04a13bb03749048fdc7
thejanthrimanna <thejan.thrimanna@ikman.lk>
2021-03-05 18:26:37 +0530

    Token config changed

--- app/src/main/java/com/thejan/otrium_android/config/AuthorizationInterceptor.kt
+++ app/src/main/java/com/thejan/otrium_android/config/AuthorizationInterceptor.kt
@@ -1,16 +1,21 @@
 package com.thejan.otrium_android.config
 
+import android.R.attr.path
 import com.thejan.otrium_android.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
+
 
 /**
  * Should add the Personal access token from GitHub here
  */
 class AuthorizationInterceptor() : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
+
+        val token = BuildConfig.TOKEN.substring(BuildConfig.TOKEN.lastIndexOf("/") + 1)
+
         val request = chain.request().newBuilder()
-            .addHeader("Authorization", "Bearer " + BuildConfig.TOKEN)
+            .addHeader("Authorization", "Bearer $token")
             .build()
 
         return chain.proceed(request)
